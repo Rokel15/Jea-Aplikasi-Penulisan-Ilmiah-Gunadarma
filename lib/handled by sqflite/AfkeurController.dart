d5566205c8a6afbc586bed092f193bdfc7505634
Jabar <violetta.je.vymmiy15@gmail.com>
2023-05-04 07:30:20 +0700

    db controller

--- lib/handled by sqflite/AfkeurController.dart
+++ lib/handled by sqflite/AfkeurController.dart
@@ -11,9 +11,29 @@ class AfkeurController extends GetxController{
 
   var AfkeurList = <AfkeurModel>[].obs;
 
+  //get all the data from table
+  void getAfkeurData() async{
+    List<Map<String, dynamic>> AfkeurData = await DB.query();
+    AfkeurList.assignAll(AfkeurData.map((data) => new AfkeurModel.fromJson(data)).toList());
+  }
+
   /*input atau tambah data*/
   Future<int> addAfkeur({required AfkeurModel? afkeurModel}) async{
     return await DB.insert(afkeurModel!);
   }
 
+  //delete
+  void delete(AfkeurModel afkeurModel) async{
+    DB.delete(afkeurModel);
+  }
+
+  //update kondisi menjadi 0
+  void kondisi0(int id) async{
+    await DB.updateKondisi0(id);
+  }
+
+  //update kondisi menjadi 1
+  void kondisi1(int id) async{
+    await DB.updateKondisi0(id);
+  }
 }
